8713d6b8e7ed7a35aae300180c2be4a26af61008
lufranca <lucyano_spfc@hotmail.com>
2021-08-20 16:37:23 -0300

    CRUD para Cliente implementado

---
+++ src/main/java/com/luciano/os/dtos/ClienteDTO.java
@@ -0,0 +1,70 @@
+package com.luciano.os.dtos;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import com.luciano.os.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	
+	@NotEmpty(message = "Campo NOME é obrigario!")
+	private String nome;
+
+	@CPF
+	@NotEmpty(message = "Campo CPF é obrigario!")
+	private String cpf;
+	
+	@NotEmpty(message = "Campo TELEFONE é obrigario!")
+	private String telefone;
+
+	public ClienteDTO() {
+		super();
+	}
+
+	public ClienteDTO(Cliente obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.telefone = obj.getTelefone();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+}
--- src/main/java/com/luciano/os/services/ClienteService.java
+++ src/main/java/com/luciano/os/services/ClienteService.java
@@ -8,46 +8,45 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.luciano.os.domain.Cliente;
 import com.luciano.os.domain.Pessoa;
-import com.luciano.os.domain.Tecnico;
-import com.luciano.os.dtos.TecnicoDTO;
+import com.luciano.os.dtos.ClienteDTO;
+import com.luciano.os.repositories.ClienteRepository;
 import com.luciano.os.repositories.PessoaRepository;
-import com.luciano.os.repositories.TecnicoRepository;
 import com.luciano.os.services.exception.DataIntegratyViolationException;
 import com.luciano.os.services.exception.ObjectNotFoundException;
 
 @Service
-public class TecnicoService {
+public class ClienteService {
 
 	@Autowired
-	private TecnicoRepository repository;
-	
-	@Autowired
-	private PessoaRepository pessoaRepository;	
+	private ClienteRepository repository;
 
-	public Tecnico findById(Integer id) {
-		Optional<Tecnico> obj = repository.findById(id);
+	@Autowired
+	private PessoaRepository pessoaRepository;
+
+	public Cliente findById(Integer id) {
+		Optional<Cliente> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + ", Tipo: " + Tecnico.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 
-	public List<Tecnico> findAll() {
+	public List<Cliente> findAll() {
 		return repository.findAll();
 	}
 
-	public Tecnico create(TecnicoDTO objDTO) {
+	public Cliente create(ClienteDTO objDTO) {
 		if (findByCPF(objDTO) != null) {
 			throw new DataIntegratyViolationException("CPF ja cadastrado na base de dados!");
 		}
 
-		return repository.save(new Tecnico(null, objDTO.getNome(), objDTO.getCpf(), objDTO.getTelefone()));
+		return repository.save(new Cliente(null, objDTO.getNome(), objDTO.getCpf(), objDTO.getTelefone()));
 	}
-	
 
-	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
-		Tecnico oldObj = findById(id);
-		
-		if(findByCPF(objDTO) != null && findByCPF(objDTO).getId() != id) {
+	public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
+		Cliente oldObj = findById(id);
+
+		if (findByCPF(objDTO) != null && findByCPF(objDTO).getId() != id) {
 			throw new DataIntegratyViolationException("CPF ja cadastrado na base de dados!");
 		}
 		oldObj.setNome(objDTO.getNome());
@@ -55,24 +54,21 @@ public class TecnicoService {
 		oldObj.setTelefone(objDTO.getTelefone());
 		return repository.save(oldObj);
 	}
-	
+
 	public void delete(Integer id) {
-		Tecnico obj = findById(id);
-		if(obj.getList().size() > 0) {
-			throw new DataIntegratyViolationException("Tecnico possui ordens de serviço, não pode ser deletado!");
+		Cliente obj = findById(id);
+		if (obj.getList().size() > 0) {
+			throw new DataIntegratyViolationException("Cliente possui ordens de serviço, não pode ser deletado!");
 		}
 		repository.deleteById(id);
 	}
 
-
-	public Pessoa findByCPF(TecnicoDTO objDTO) {
+	public Pessoa findByCPF(ClienteDTO objDTO) {
 		Pessoa obj = pessoaRepository.findByCPF(objDTO.getCpf());
 		if (obj != null) {
 			return obj;
 		}
 		return null;
 	}
-
-
 
 }
